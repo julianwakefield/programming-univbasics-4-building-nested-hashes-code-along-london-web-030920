e1395b2394cb7118f8b982d2c1bbf6d510a339e0
Julian Wakefield <julianwakefield@outlook.com>
2020-02-28 22:50:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch
-      :matriarch
-      :hero
-      :hero_friends#your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       },
     :capulet => {
-      :patriarch
-      :matriarch
-      :heroine
-      :heroine_friends#your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }
     }
 
